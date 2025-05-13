634bf823ce02fcae9350950c08affa3a4c8a7396
Robert Nelson <robertcnelson@gmail.com>
2025-05-13 16:14:30 -0500

    ci: branch: pull from everywhere
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --rebase --no-edit https://github.com/beagleboard/BeagleV-Fire-ubuntu.git v6.1.x-Beagle
+git pull --rebase --no-edit https://openbeagle.org/beaglev-fire/BeagleV-Fire-ubuntu.git v6.1.x-Beagle
+git pull --rebase --no-edit https://gitlab.com/RobertCNelson/beaglev-fire-ubuntu.git v6.1.x-Beagle
