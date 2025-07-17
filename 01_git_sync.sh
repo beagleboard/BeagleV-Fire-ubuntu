55db7b9c57efc4f937c3cb531c4f985900785113
Robert Nelson <robertcnelson@gmail.com>
2025-07-17 15:57:33 -0500

    hss: (hart-software-services) upgrade to v2024.02.01
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -3,7 +3,7 @@
 GIT_DEPTH="20"
 GCC_VERSION="11.4.0"
 
-HSS_BRANCH="v2023.09"
+HSS_BRANCH="v2024.02.1"
 HSS_REPO="https://github.com/polarfire-soc/hart-software-services.git"
 
 #UBOOT_BRANCH="v2023.02-BeagleV-Fire"
