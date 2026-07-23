8d75728ad03005e54f43b70b39862afa8dda856b
Robert Nelson <robertcnelson@gmail.com>
2026-07-23 12:31:38 -0500

    ci: only build riscv64 on gitlab.com
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 01_git_sync.sh
+++ 01_git_sync.sh
@@ -8,24 +8,16 @@ HSS_BRANCH="v2025.07"
 HSS_REPO="https://github.com/polarfire-soc/hart-software-services.git"
 CI_HSS_REPO="https://forgejo.gfnd.rcn-ee.org:3000/Microchip/hart-software-services.git"
 
-#UBOOT_BRANCH="v2023.02-BeagleV-Fire"
-#UBOOT_REPO="https://openbeagle.org/beaglev-fire/beaglev-fire-u-boot.git"
 UBOOT_BRANCH="linux4microchip+fpga-2025.10"
 UBOOT_REPO="https://github.com/linux4microchip/u-boot-mchp.git"
 CI_UBOOT_REPO="https://forgejo.gfnd.rcn-ee.org:3000/Microchip/u-boot-mchp.git"
 
 DT_BRANCH="v6.12.x-Beagle"
 DT_REPO="https://github.com/beagleboard/BeagleBoard-DeviceTrees.git"
-#DT_REPO="git@openbeagle.org:beagleboard/BeagleBoard-DeviceTrees.git"
 
 LINUX_BRANCH="linux4microchip+fpga-2025.10"
 LINUX_REPO="https://github.com/linux4microchip/linux.git"
 CI_LINUX_REPO="https://forgejo.gfnd.rcn-ee.org:3000/Microchip/linux.git"
-#LINUX_REPO="https://openbeagle.org/beaglev-fire/beaglev-fire-linux.git"
-#LINUX_REPO="git@openbeagle.org:beaglev-fire/beaglev-fire-linux.git"
-
-#LINUX_BRANCH="master"
-#LINUX_REPO="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
 
 if [ ! -f .ci-native-gcc ] ; then
 	if [ ! -f ./mirror/x86_64-gcc-${GCC_VERSION}-nolibc-riscv64-linux.tar.xz ] ; then
@@ -46,8 +38,8 @@ fi
 touch .gitlab-runner
 
 if [ -f .gitlab-runner ] ; then
-	echo "git clone -b ${HSS_BRANCH} ${CI_HSS_REPO} ./hart-software-services/ --depth=${GIT_DEPTH}"
-	git clone -b ${HSS_BRANCH} ${CI_HSS_REPO} ./hart-software-services/ --depth=${GIT_DEPTH}
+	echo "git clone -b ${HSS_BRANCH} ${CI_HSS_REPO} ./hart-software-services/ --depth=5"
+	git clone -b ${HSS_BRANCH} ${CI_HSS_REPO} ./hart-software-services/ --depth=5
 else
 	echo "git clone -b ${HSS_BRANCH} ${HSS_REPO} ./hart-software-services/ --depth=${GIT_DEPTH}"
 	git clone -b ${HSS_BRANCH} ${HSS_REPO} ./hart-software-services/ --depth=${GIT_DEPTH}
@@ -58,8 +50,8 @@ if [ -d ./u-boot ] ; then
 fi
 
 if [ -f .gitlab-runner ] ; then
-	echo "git clone -b ${UBOOT_BRANCH} ${CI_UBOOT_REPO} ./u-boot/ --depth=${GIT_DEPTH}"
-	git clone -b ${UBOOT_BRANCH} ${CI_UBOOT_REPO} ./u-boot/ --depth=${GIT_DEPTH}
+	echo "git clone -b ${UBOOT_BRANCH} ${CI_UBOOT_REPO} ./u-boot/ --depth=5"
+	git clone -b ${UBOOT_BRANCH} ${CI_UBOOT_REPO} ./u-boot/ --depth=5
 else
 	echo "git clone -b ${UBOOT_BRANCH} ${UBOOT_REPO} ./u-boot/ --depth=${GIT_DEPTH}"
 	git clone -b ${UBOOT_BRANCH} ${UBOOT_REPO} ./u-boot/ --depth=${GIT_DEPTH}
@@ -77,8 +69,8 @@ if [ -d ./linux ] ; then
 fi
 
 if [ -f .gitlab-runner ] ; then
-	echo "git clone -b ${LINUX_BRANCH} ${CI_LINUX_REPO} ./linux/ --depth=${GIT_DEPTH}"
-	git clone --reference-if-able /opt/linux-src/ -b ${LINUX_BRANCH} ${CI_LINUX_REPO} ./linux/ --depth=${GIT_DEPTH}
+	echo "git clone -b ${LINUX_BRANCH} ${CI_LINUX_REPO} ./linux/ --depth=5"
+	git clone --reference-if-able /opt/linux-src/ -b ${LINUX_BRANCH} ${CI_LINUX_REPO} ./linux/ --depth=5
 else
 	echo "git clone -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/ --depth=${GIT_DEPTH}"
 	git clone --reference-if-able ~/linux-src/ -b ${LINUX_BRANCH} ${LINUX_REPO} ./linux/ --depth=${GIT_DEPTH}
