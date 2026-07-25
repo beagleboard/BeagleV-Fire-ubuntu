c915193427023abcaea5a87648b9d1c6cdc66c0f
Robert Nelson <robertcnelson@gmail.com>
2026-07-25 17:07:00 -0500

    ci: generate *.img.xz.yml.txt file bb-imager-rs needs
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 07_create_sdcard_img.sh
+++ 07_create_sdcard_img.sh
@@ -40,18 +40,29 @@ if [[ -f ../.gitlab-runner ]] ; then
 		datestamp=$(< .datestamp)
 		image_name="beaglev-fire-debian-13-iot-v6.12-riscv64-${datestamp}-4gb"
 		cp -v ./images/sdcard.img ./images/${image_name}.img
+		TIME=${datestamp}
 	else
 		image_name="sdcard"
+		TIME=$(date +%Y-%m-%d)
 	fi
 else
 	image_name="sdcard"
+	TIME=$(date +%Y-%m-%d)
+fi
+
+echo "- name: BeagleV-Fire Debian 13 v6.12.x" > ./images/${image_name}.img.xz.yml.txt
+echo "  description: Debian 13 (Trixie) with no desktop environment for BeagleV-Fire based on Microchip Polarfire MPFS025T SoC FPGA running Microchip's linux 6.12" >> ./images/${image_name}.img.xz.yml.txt
+echo "  icon: https://media.githubusercontent.com/media/beagleboard/bb-imager-rs/refs/heads/main/assets/os/debian.png" >> ./images/${image_name}.img.xz.yml.txt
+echo "  url: https://files.beagle.cc/file/beagleboard-public-2021/images/${image_name}.img.xz" >> ./images/${image_name}.img.xz.yml.txt
+if [ -f /usr/bin/bmaptool ] ; then
+	echo "  bmap: https://raw.githubusercontent.com/beagleboard/distros/refs/heads/main/bmap-temp/${image_name}.bmap" >> ./images/${image_name}.img.xz.yml.txt
 fi
 
 extract_size=$(du -b ./images/${image_name}.img | awk '{print $1}')
-echo "  extract_size: ${extract_size}" > ./images/image.yml.txt
+echo "  extract_size: ${extract_size}" >> ./images/${image_name}.img.xz.yml.txt
 
 extract_sha256=$(sha256sum ./images/${image_name}.img | awk '{print $1}')
-echo "  extract_sha256: ${extract_sha256}" >> ./images/image.yml.txt
+echo "  extract_sha256: ${extract_sha256}" >> ./images/${image_name}.img.xz.yml.txt
 
 if [ -f /usr/bin/bmaptool ] ; then
 	if [ -f ./images/${image_name}.bmap ] ; then
@@ -70,14 +81,15 @@ if [ ! -f ./images/${image_name}.img.xz ]; then
 fi
 
 image_download_size=$(du -b ./images/${image_name}.img.xz | awk '{print $1}')
-echo "  image_download_size: ${image_download_size}" >> ./images/image.yml.txt
+echo "  image_download_size: ${image_download_size}" >> ./images/${image_name}.img.xz.yml.txt
 
 image_download_sha256=$(sha256sum ./images/${image_name}.img.xz | awk '{print $1}')
-echo "  image_download_sha256: ${image_download_sha256}" >> ./images/image.yml.txt
+echo "  image_download_sha256: ${image_download_sha256}" >> ./images/${image_name}.img.xz.yml.txt
 
-TIME=$(date +%Y-%m-%d)
-
-echo "  release_date: '${TIME}'" >> ./images/image.yml.txt
-echo "  init_format: sysconf" >> ./images/image.yml.txt
+echo "  release_date: '${TIME}'" >> ./images/${image_name}.img.xz.yml.txt
+echo "  init_format: sysconf" >> ./images/${image_name}.img.xz.yml.txt
+echo "    devices:" >> ./images/${image_name}.img.xz.yml.txt
+echo "      - beaglev-fire" >> ./images/${image_name}.img.xz.yml.txt
+echo "      - recommended" >> ./images/${image_name}.img.xz.yml.txt
 
 #
